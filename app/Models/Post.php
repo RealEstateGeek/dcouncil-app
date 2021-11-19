6d9bdb65a432ad483994dd93c085c92a787aa120
David Council <DCouncilCSU@gmail.com>
2021-11-19 15:38:35 -0700

    configured DB seeder, updated Post FKs for User

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,8 +9,17 @@ class Post extends Model
 {
     use HasFactory;
 
+    protected $guarded = [
+        'id',
+    ];
+
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
