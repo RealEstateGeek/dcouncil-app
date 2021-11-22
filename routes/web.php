9bc29f713e1284020f6170b7aefe52bbbb7c8cf4
David Council <DCouncilCSU@gmail.com>
2021-11-22 16:03:21 -0700

    got dropdown formatted and working

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/posts', function () {
     return view('posts', [
         'posts' => Post::latest()->with('category', 'user')->get(),
+        'categories' => Category::all(),
     ]);
 });
 
@@ -34,6 +35,8 @@ Route::get('/posts/{post}', function(Post $post) {
 Route::get('/categories/{category:slug}', function(Category $category) {
     return view('posts', [
         'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all(),
     ]);
 });
 
@@ -41,5 +44,6 @@ Route::get('/categories/{category:slug}', function(Category $category) {
 Route::get('/users/{user}', function (User $user) {
     return view('posts', [
         'posts' => $user->posts,
+        'categories' => Category::all(),
     ]);
 });
