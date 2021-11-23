b2c8bdb8f4752f806537cf8b868342193abd8e79
David Council <DCouncilCSU@gmail.com>
2021-11-23 14:03:11 -0700

    basic form validation for register

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/posts', [PostController::class, 'index'])->name('home');
 Route::get('/posts/{post}', [PostController::class, 'view']);
+
+Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);
