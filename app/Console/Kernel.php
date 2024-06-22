4b46c31c9c06209f72b57766d9accd32be68b815
Mitch Reynolds <103149930+RealEstateGeek@users.noreply.github.com>
2024-06-22 03:17:40 -0600

    Update Kernel.php
    what is it

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -39,3 +39,17 @@ class Kernel extends ConsoleKernel
         require base_path('routes/console.php');
     }
 }
+
+#sup
+<!https://www.etsy.com/listing/1740613299/the-treachery-of-images-canvas-wall-art?gpla=1&gao=1&&utm_source=google&utm_medium=cpc&utm_campaign=shopping_us_b-home_and_living-home_decor-wall_decor-wall_hangings-other&utm_custom1=_k_CjwKCAjw7NmzBhBLEiwAxrHQ-bUA2ZGPUfLTa9x2vl1vhUzh0ffqq0_DpgoC2lzotLk6lBFCYmcmSBoCySMQAvD_BwE_k_&utm_content=go_1843970812_75209275372_346398223084_pla-305562556206_c__1740613299_12768591&utm_custom2=1843970812&gad_source=1&gclid=CjwKCAjw7NmzBhBLEiwAxrHQ-bUA2ZGPUfLTa9x2vl1vhUzh0ffqq0_DpgoC2lzotLk6lBFCYmcmSBoCySMQAvD_BwE>
+<html>
+<body>
+
+<h1>My First PHP Page</h1>
+
+<?php
+echo "what is it";
+?>
+
+</body>
+</html>
