6097bf339de97eecec70cfdc3018b8d390aa7cde
Mitch Reynolds <103149930+RealEstateGeek@users.noreply.github.com>
2024-06-25 23:04:00 -0600

    Update Kernel.php
    mitch is

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,67 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        $schedule->call(function () {
+            Log::info('This is a scheduled task running!');
+        })->hourly();
+    }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__.'/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
+php artisan make:command PrintMessage
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class PrintMessage extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:printmessage';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Print a message to the console';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->info('Mitch is cooler than Dave.');
+        return 0;
+    }
+}
 
 class Kernel extends ConsoleKernel
 {
